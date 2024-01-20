2db898b0c10e7e28c0f0518ad466789115eaf44f
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-01-20 15:16:14 -0300

    feat: 🎸 Show data about the user in the header

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,5 +1,8 @@
+import { useQuery } from '@tanstack/react-query'
 import { Building, ChevronDown, LogOut } from 'lucide-react'
 
+import { getManagedRestaurant } from '@/api/get-managed-restaurant.ts'
+import { getProfile } from '@/api/get-profile.ts'
 import { Button } from '@/components/ui/button'
 import {
     DropdownMenu,
@@ -9,29 +12,40 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 export function AccountMenu() {
+
+    const { data: profile} = useQuery({
+        queryKey: ['profile'],
+        queryFn: getProfile
+    })
+
+    const { data: managedRestaurant} = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant
+    })
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="outline" className="flex items-center gap-2 select-none">
-                    Pizza Shop
+                <Button variant="outline" className="flex select-none items-center gap-2">
+                    {managedRestaurant?.name}
                     <ChevronDown />
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className="w-56">
                 <DropdownMenuLabel className="flex flex-col">
-                    <span>Gabriel S. Cordeiro</span>
-                    <span className="text-sm font-normal text-muted-foreground">gabrielcordeiro.dev@gmail.com</span>
+                    <span>{profile?.name}</span>
+                    <span className="text-sm font-normal text-muted-foreground">{profile?.email}</span>
                 </DropdownMenuLabel>
 
                 <DropdownMenuSeparator />
 
                 <DropdownMenuItem className="cursor-pointer">
-                    <Building className="w-4 h-4 mr-2" />
+                    <Building className="mr-2 size-4" />
                     <span>Store profile</span>
                 </DropdownMenuItem>
 
                 <DropdownMenuItem className="cursor-pointer text-rose-500  dark:text-rose-400">
-                    <LogOut className="w-4 h-4 mr-2" />
+                    <LogOut className="mr-2 size-4" />
                     <span>Logout</span>
                 </DropdownMenuItem>
             </DropdownMenuContent>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,6 @@ import axios from 'axios'
 import { env } from '@/env.ts'
 
 export const api = axios.create({
-    baseURL: env.VITE_API_URL
+    baseURL: env.VITE_API_URL,
+    withCredentials: true
 })
