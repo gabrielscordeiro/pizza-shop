7f6a30d24c0e83791be5633991b8ec8728f710ac
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-01-25 21:33:02 -0300

    feat: 🎸 Automatic logout

--- src/pages/_layouts/painel.tsx
+++ src/pages/_layouts/painel.tsx
@@ -1,13 +1,39 @@
-import { Outlet } from 'react-router-dom'
+import { isAxiosError } from 'axios'
+import { useEffect } from 'react'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { Header } from '@/components/header'
+import { api } from '@/lib/axios.ts'
 
 export function PainelLayout() {
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const interceptorId = api.interceptors.response.use(
+            response => response,
+            error => {
+                if(isAxiosError(error)) {
+                    const status  = error.response?.status
+                    const code = error.response?.data.code
+
+                    if(status === 401 && code === 'UNAUTHORIZED') {
+                        navigate('sign-in', { replace: true })
+                    }
+                }
+            }
+        )
+
+        return () => {
+            api.interceptors.response.eject(interceptorId)
+        }
+    }, [navigate])
+
+
     return (
-        <div className="flex flex-col min-h-screen antialiased">
+        <div className="flex min-h-screen flex-col antialiased">
             <Header />
 
-            <div className="flex flex-col flex-1 gap-4 p-8 pt-6">
+            <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
                 <Outlet />
             </div>
         </div>
