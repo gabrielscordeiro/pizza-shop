94cb6fcda7cb3f5890141b71fca8af65d3cf3ddf
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-01-29 21:53:57 -0300

    test: 💍 Test environment setup

---
+++ src/components/order-status.spec.tsx
@@ -0,0 +1,4 @@
+
+test('1 + 1 equals 2', () => {
+    expect(1 + 1).toEqual(2)
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import path from "node:path"
+import path from 'node:path'
 
-
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import type { InlineConfig } from 'vitest'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        }, 
     },
-  }
+    test: {
+        globals: true,
+        setupFiles: ['./test/setup.ts'],
+        environment: 'happy-dom'
+    }
+} as UserConfig & {
+  test: InlineConfig
 })
