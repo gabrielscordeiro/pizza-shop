111411ff1f4317478b0ff72da94cf996ebb63d66
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-02-04 17:29:01 -0300

    test: 💍 Register restaurant mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,9 +1,10 @@
 import { setupWorker } from 'msw/browser'
 
+import { registerRestaurantMock } from '@/api/mocks/register-restaurant-mock'
 import { signInMock } from '@/api/mocks/sign-in-mock.ts'
 import { env } from '@/env'
 
-export const worker = setupWorker(signInMock)
+export const worker = setupWorker(signInMock, registerRestaurantMock)
 
 export async function enableMSW()  {
 
