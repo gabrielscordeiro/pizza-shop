fac059a2e9c7c7d69ab22f7e0d6a7dbfda28fa40
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-02-04 16:23:26 -0300

    test: 💍 Authentication mock

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,8 +1,9 @@
 import { setupWorker } from 'msw/browser'
 
+import { signInMock } from '@/api/mocks/sign-in-mock.ts'
 import { env } from '@/env'
 
-export const worker = setupWorker()
+export const worker = setupWorker(signInMock)
 
 export async function enableMSW()  {
 
