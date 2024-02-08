cb91eb5e60a0930bb5614825c7ea13bbb8c820a8
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-02-07 21:59:27 -0300

    test: 💍 E2E: Orders list

---
+++ test/orders.e2e-spec.ts
@@ -0,0 +1,70 @@
+import { expect, test } from '@playwright/test'
+
+test('list orders', async ({ page }) => {
+    await page.goto('/orders', { waitUntil: 'networkidle' })
+
+    expect(page.getByRole('cell', { name: 'Customer 1', exact: true })).toBeVisible()
+    expect(page.getByRole('cell', { name: 'Customer 10' })).toBeVisible()
+})
+
+
+test('paginate orders', async ({ page }) => {
+    await page.goto('/orders', { waitUntil: 'networkidle' })
+
+    await page.getByRole('button', { name: 'Next page' }).click()
+
+    expect(page.getByRole('cell', { name: 'Customer 11', exact: true })).toBeVisible()
+    expect(page.getByRole('cell', { name: 'Customer 20' })).toBeVisible()
+
+
+    await page.getByRole('button', { name: 'Last page' }).click()
+
+    expect(page.getByRole('cell', { name: 'Customer 51', exact: true })).toBeVisible()
+    expect(page.getByRole('cell', { name: 'Customer 60' })).toBeVisible()
+
+
+    await page.getByRole('button', { name: 'Previous page' }).click()
+
+    expect(page.getByRole('cell', { name: 'Customer 41', exact: true })).toBeVisible()
+    expect(page.getByRole('cell', { name: 'Customer 50' })).toBeVisible()
+
+
+    await page.getByRole('button', { name: 'First page' }).click()
+
+    expect(page.getByRole('cell', { name: 'Customer 1', exact: true })).toBeVisible()
+    expect(page.getByRole('cell', { name: 'Customer 10' })).toBeVisible()
+})
+
+test('filter by order id', async ({ page }) => {
+    await page.goto('/orders', { waitUntil: 'networkidle' })
+
+    await page.getByPlaceholder('Order ID').fill('order-11')
+
+    await page.getByRole('button', { name: 'Filter results' }).click()
+
+    expect(page.getByRole('cell', { name: 'order-11' })).toBeVisible()
+})
+
+test('filter by customer name', async ({ page }) => {
+    await page.goto('/orders', { waitUntil: 'networkidle' })
+
+    await page.getByPlaceholder('Customer name').fill('Customer 11')
+
+    await page.getByRole('button', { name: 'Filter results' }).click()
+
+    expect(page.getByRole('cell', { name: 'Customer 11' })).toBeVisible()
+})
+
+test('filter by status', async ({ page }) => {
+    await page.goto('/orders', { waitUntil: 'networkidle' })
+
+    await page.getByRole('combobox').click()
+    await page.getByLabel('Pending').click()
+
+    await page.getByRole('button', { name: 'Filter results' }).click()
+
+    const tableRows = await page.getByRole('cell', { name: 'Pending' }).all()
+
+    expect(tableRows).toHaveLength(10)
+
+})
