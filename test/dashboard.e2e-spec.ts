e6cd68fe541878c8cb01fbea75787faa7054bbed
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-02-07 21:43:57 -0300

    test: 💍 E2E: Dashboard metrics

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { expect, test } from '@playwright/test'
+
+test('display day orders amount metric', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
+
+    expect(page.getByText('20', { exact: true })).toBeVisible()
+    expect(page.getByText('--5% compared to yesterday')).toBeVisible()
+
+})
+
+test('display month orders amount metric', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
+
+    expect(page.getByText('200', { exact: true })).toBeVisible()
+    expect(page.getByText('7% compared to last month')).toBeVisible()
+
+})
+
+test('display month canceled orders amount metric', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
+
+    expect(page.getByText('5', { exact: true })).toBeVisible()
+    expect(page.getByText('-5% compared to last month')).toBeVisible()
+
+})
+
+
+test('display month revenue  metric', async ({ page }) => {
+    await page.goto('/', { waitUntil: 'networkidle' })
+
+    expect(page.getByText('$200.00')).toBeVisible()
+    expect(page.getByText('+10% compared to last month')).toBeVisible()
+
+})
